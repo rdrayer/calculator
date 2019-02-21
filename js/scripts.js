88a176af10cb3896f45d33480cd02ead4844346b
Student <example@example.com>
2019-02-20 20:45:39 -0800

    branch for triangle tracker proj

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
-var calc = function(number1, number2, operator) {
-  if (operator === "add") {
-    return number1 + number2;
-  } else if (operator === "subtract") {
-    return number1 - number2;
-  } else if (operator === "multiply") {
-    return number1 * number2;
-  } else {
-    return number1 / number2;
+var tri = function(number1, number2, number3) {
+
+    if (number1 === number2 && number2 === number3 && number1 === number3) {
+      return "Equilateral";
+    } else if ((number1 === number2) || (number1 === number3) || (number2 === number3)) {
+      return "Isoceles";
+    } else if ((number1 != number2) || (number1 != number3) || (number2 != number3)) {
+      return "Scalene";
+    } else if (((number1 + number2) <= number3)) {
+      return "Not a triangle";
+    }
+
   }
-}
+
 
 $(document).ready(function() {
-  $("form#calculator").submit(function() {
+  $("form#triangle").submit(function() {
     event.preventDefault();
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
-    var result = calc(number1, number2, operator);
+    var number3 = parseInt($("#input3").val());
+    var result = tri(number1, number2, number3);
     $("#output").text(result);
   });
 });
